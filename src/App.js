f4e957b98a29d3369f04d68e2fb5485d09e78c57
gumarengo <gu_marengo@hotmail.com>
2022-08-28 15:31:22 -0300

    set states for entire application

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import StartScreen from "./components/StartScreen/StartScreen";
 import Game from "./components/GameScreen/Game";
 import GameOver from "./components/GameOverScreen/GameOver";
 
+import SilvioSantos from "./data/silvio.png";
+
 const stages = [
   { id: 1, name: "start" },
   { id: 2, name: "game" },
@@ -18,12 +20,47 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const [guessedLetters, setGuessedLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [guesses, setGuesses] = useState(5);
+  const [score, setScore] = useState(0);
+
+  const pickWordAndCategory = () => {
+    const categories = Object.keys(words);
+    //pega a categoria
+    const category =
+      categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    //pega uma palavra da categoria acima
+    const word =
+      words[category][Math.floor(Math.random() * words[category].length)];
+
+    return { word, category };
+  };
+
   const startGame = () => {
+    const { word, category } = pickWordAndCategory();
+
+    //cria o array de letras de word
+    let wordLetters = word.split("");
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+
+    console.log(word, category);
+    console.log(wordLetters);
+
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(wordLetters);
+
     setGameStage(stages[1].name);
   };
 
-  const verifyLetter = () => {
-    setGameStage(stages[2].name);
+  const verifyLetter = (letter) => {
+    console.log(letter);
   };
 
   const retry = () => {
@@ -31,10 +68,26 @@ function App() {
   };
 
   return (
-    <div className="App">
-      {gameStage === "start" && <StartScreen startGame={startGame} />}
-      {gameStage === "game" && <Game verifyLetter={verifyLetter} />}
-      {gameStage === "end" && <GameOver retry={retry} />}
+    <div>
+      <div className="App">
+        {gameStage === "start" && <StartScreen startGame={startGame} />}
+        {gameStage === "game" && (
+          <Game
+            verifyLetter={verifyLetter}
+            pickedWord={pickedWord}
+            pickedCategory={pickedCategory}
+            letters={letters}
+            guessedLetters={guessedLetters}
+            wrongLetters={wrongLetters}
+            guesses={guesses}
+            score={score}
+          />
+        )}
+        {gameStage === "end" && <GameOver retry={retry} />}
+      </div>
+      <div className="image">
+        <img src={SilvioSantos} alt="Silvio Santos" />
+      </div>
     </div>
   );
 }
--- src/components/GameOverScreen/GameOver.js
+++ src/components/GameOverScreen/GameOver.js
@@ -4,6 +4,7 @@ const GameOver = ({ retry }) => {
   return (
     <div>
       <h1>Game Over</h1>
+
       <button onClick={retry}>Jogar novamente</button>
     </div>
   );
